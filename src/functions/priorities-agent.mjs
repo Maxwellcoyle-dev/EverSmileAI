febc5ddfbb8c6b528f76091a1ef1133c1c7a72fe
Maxwell Henderson <maxwellc.henderson@gmail.com>
2025-08-23 19:36:36 -0600

    testing

--- src/functions/priorities-agent.mjs
+++ src/functions/priorities-agent.mjs
@@ -2,11 +2,11 @@ import googleAuth from "../utilities/google-auth.mjs";
 
 const priorityAgent = async (event) => {
   console.log(event);
-  
+
   try {
     const googleAuthResponse = await googleAuth();
     console.log("Google Auth Response:", googleAuthResponse);
-    
+
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -14,18 +14,18 @@ const priorityAgent = async (event) => {
         awsIdentity: googleAuthResponse.awsIdentity,
         drives: googleAuthResponse.drives,
         sampleFiles: googleAuthResponse.sampleFiles,
-        event: event
+        event: event,
       }),
     };
   } catch (error) {
     console.error("Error in priorityAgent:", error);
-    
+
     return {
       statusCode: 500,
       body: JSON.stringify({
         message: "Error occurred",
         error: error.message,
-        event: event
+        event: event,
       }),
     };
   }
